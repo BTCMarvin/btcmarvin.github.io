bc3b95fb18f1e51c1a565f2633d918d5b197f533
Keks_Bombe <m.lohss04@gmail.com>
2024-03-21 10:14:12 +0100

    format backroll

--- customTextWidget/Webcomponent.js
+++ customTextWidget/Webcomponent.js
@@ -6,7 +6,7 @@
         border: 1px solid #ccc;
         padding: 10px;
         margin-bottom: 10px;
-        height: 150px; /* Adjust based on need */
+        height: 100%; /* Adjust based on need */
         overflow: auto;
         width: 100%; /* Ensure it takes the full width of the container */
         background-color: white; /* Explicitly set background to white */
